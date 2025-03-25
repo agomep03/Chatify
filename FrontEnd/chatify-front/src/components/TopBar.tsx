0355e39e12479e6271b07ae01e893be9bcd30159
alicia <agomep03@estudiantes.unileon.es>
2025-03-25 19:56:03 +0100

    Empezado top bar

---
+++ FrontEnd/chatify-front/src/components/TopBar.tsx
@@ -0,0 +1,27 @@
+import { AppBar, Toolbar, Typography, Avatar } from '@mui/material';
+import Logo from '../assets/Logo.png'; 
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'; 
+
+const TopBar: React.FC = () => {
+  return (
+    <AppBar position="fixed">
+      <Toolbar>
+        <Avatar 
+          src={Logo}
+          alt="Chatify Logo"
+          sx={{ mr: 2 }} 
+        />
+        <Typography variant="h6" component="div">
+          Chatify
+        </Typography>
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <IconButton color="inherit">
+            <AccountCircleIcon />
+          </IconButton>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default TopBar;
---
+++ FrontEnd/chatify-front/src/pages/ChatPage.tsx
@@ -0,0 +1,12 @@
+import MainLayout from '../layouts/MainLayout';
+
+const ChatPage: React.FC = () => {
+  return (
+    <MainLayout>
+      <h1>Chat</h1>
+      <p>Aquí irá el chat...</p>
+    </MainLayout>
+  );
+};
+
+export default ChatPage;
