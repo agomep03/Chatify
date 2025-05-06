3eb32ea2a8155b63d38c3f8026c08613db6f6927
aorted010 <aorted01@estudiantes.unileon.es>
2025-05-06 18:48:51 +0200

    Chat mejorado

--- BackEnd/src/routes/chat_routes.py
+++ BackEnd/src/routes/chat_routes.py
@@ -1,9 +1,8 @@
-# src/routes/chat_routes.py
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 from src.controllers.chat_controller import consult_IA
 
 router = APIRouter()
 
 @router.post("/ask-music-question/")
-async def ask_music_question_endpoint(question: str):
+async def ask_music_question_endpoint(question: str = Query(...)):
     return await consult_IA(question)
