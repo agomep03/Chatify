2482f957931cd117a9d4457a4766feda68f0d39c
alicia <agomep03@estudiantes.unileon.es>
2025-05-06 18:48:04 +0200

    Add Nav Menu component

---
+++ FrontEnd/chatify-front/src/components/NavMenu.tsx
@@ -0,0 +1,11 @@
+import {Box } from '@mui/material';
+
+const NavMenu: React.FC = () => {
+  return (
+    <Box sx={{ width: 240, backgroundColor: '#7c7c7c', p: 2 }}>
+        <p>lateral...</p>
+    </Box>
+  );
+};
+
+export default NavMenu;
