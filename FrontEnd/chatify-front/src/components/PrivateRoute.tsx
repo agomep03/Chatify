96f49b06af547d3ab6ccb9cae6ed1f5ab8c6b68d
MarioLopB <mlopeb04@estudiantes.unileon.es>
2025-05-07 16:43:24 +0200

    Añadir alerta al intentar entrar a la pagina principal sin logearse

--- FrontEnd/chatify-front/src/components/PrivateRoute.tsx
+++ FrontEnd/chatify-front/src/components/PrivateRoute.tsx
@@ -1,8 +1,19 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { isAuthenticated } from "../utils/auth";
+import { useEffect, useRef } from "react";
 
 const PrivateRoute: React.FC = () => {
-  return isAuthenticated() ? <Outlet /> : <Navigate to="/login" replace />;
+  const authenticated = isAuthenticated();
+  const hasAlerted = useRef(false);
+
+  useEffect(() => {
+    if (!authenticated && !hasAlerted.current) {
+      alert("No estás autenticado");
+      hasAlerted.current = true;
+    }
+  }, [authenticated]);
+
+  return authenticated ? <Outlet /> : <Navigate to="/login" replace />;
 };
 
 export default PrivateRoute;
