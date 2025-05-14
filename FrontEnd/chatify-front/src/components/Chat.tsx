838bbe88f7ecb607c9febe17b0b8c8c76c2c775e
alicia <agomep03@estudiantes.unileon.es>
2025-05-14 17:59:15 +0200

    interfaz basica de mensajes funciona, pero no esta conectado al backend

---
+++ FrontEnd/chatify-front/src/components/Chat.tsx
@@ -0,0 +1,128 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { Box, List, ListItem, ListItemText, TextField, IconButton } from '@mui/material';
+import SendIcon from '@mui/icons-material/Send';
+
+type Message = {
+  id: number;
+  text: string;
+  sender: 'user' | 'bot';
+};
+
+
+
+const Chat: React.FC = () => {
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [input, setInput] = useState('');
+
+  const listRef = useRef<HTMLUListElement>(null);
+
+  useEffect(() => {
+    if (listRef.current) {
+      listRef.current.scrollTo({
+        top: listRef.current.scrollHeight,
+        behavior: 'smooth'
+      });
+    }
+  }, [messages]); 
+
+  const handleSend = () => {
+    if (!input.trim()) return;
+
+    const userMessage: Message = {
+      id: Date.now(),
+      text: input,
+      sender: 'user',
+    };
+
+    const botMessage: Message = {
+      id: Date.now() + 1,
+      text: 'en desarrollo',
+      sender: 'bot',
+    };
+
+    setMessages((prev) => [...prev, userMessage, botMessage]);
+    setInput('');
+  };
+
+  return (
+    <Box display="flex" flexDirection="column" height="85vh" width="85vw" overflow="hidden" p={2}>
+      <Box
+        ref={listRef}
+        flexGrow={1}
+        overflow="hidden"
+        mb={2}
+        sx={{ maxHeight: '90%', overflowY: 'auto', overflowX: 'hidden' }}
+      >
+        <List>
+        {messages.map((msg) => (
+        <ListItem key={msg.id} sx={{ display: 'flex', justifyContent: msg.sender === 'user' ? 'flex-end' : 'flex-start' }}>
+          <Box
+            sx={{
+              maxWidth: '80%',
+              wordWrap: 'break-word',  // Hace que el texto se ajuste y no se desborde
+              backgroundColor: msg.sender === 'user' ? '#414141' : '#303030', //color user : bot
+              color: '#fff',
+              borderRadius:  msg.sender === 'user' ? '15px 15px 0px 15px' : '15px 15px 15px 0px', //redondeo esquinas user : bot
+              padding: '8px 12px', 
+              wordBreak: 'break-word',
+              whiteSpace: 'pre-wrap'
+            }}
+          >
+            <ListItemText
+              primary={msg.text}
+              align={msg.sender === 'user' ? 'right' : 'left'}
+              sx={{ whiteSpace: 'pre-wrap' }} 
+            />
+          </Box>
+        </ListItem>
+      ))}
+        </List>
+      </Box>
+      <Box display="flex" overflow="hidden">
+        <TextField
+          fullWidth
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Escribe un mensaje..."
+          multiline
+          minRows={1}
+          maxRows={6}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              e.preventDefault();
+              handleSend();
+              setInput('');
+            }
+          }}
+          sx={{
+            backgroundColor:'#303030',
+            resize: 'none',
+            overflow: 'auto',
+            borderRadius: "15px",
+            color: '#fff',
+            '& .MuiInputBase-input': {
+                color: '#fff', 
+            },
+            '& .MuiOutlinedInput-root': { //Eliminamos el borde y sus variaciones cuando se toca el componente
+                '& fieldset': {
+                    border: 'none',
+                },
+                '&:hover fieldset': {
+                    border: 'none',
+                },
+                '&.Mui-focused fieldset': {
+                    border: 'none',
+                },
+            },
+          }}
+    
+        />
+        <IconButton onClick={handleSend} sx={{ color: '#3be477' }}>
+          <SendIcon />
+        </IconButton>
+      </Box>
+    </Box>
+  );
+};
+
+export default Chat;
