4fccfd6b21030e13e25471af9f9b604d06612d57
alicia <agomep03@estudiantes.unileon.es>
2025-05-15 09:11:45 +0200

    update config.ts

--- FrontEnd/chatify-front/src/config.ts
+++ FrontEnd/chatify-front/src/config.ts
@@ -1,5 +1,19 @@
+// src/config.ts
+
+const isDevelopment = import.meta.env.MODE === "development";
+
+// Obtenemos y procesamos las URLs de producción
+const prodUrls: string[] = import.meta.env.VITE_API_PROD_URLS
+  ? import.meta.env.VITE_API_PROD_URLS.split(",").map((url: string) => url.trim())
+  : [];
+
 const config = {
-  apiBaseUrl: import.meta.env.VITE_API_BASE_URL || "https://chatifyback.onrender.com",
+  apiBaseUrl: isDevelopment
+    ? import.meta.env.VITE_API_BASE_URL
+    : prodUrls.length > 0
+    ? prodUrls[0] // Selecciona la primera por defecto, o podrías implementar lógica para elegir otra
+    : "", // Fallback si no hay URL definida
 };
+console.log(`API base URL configurada: ${config.apiBaseUrl || "No definida"}`);
 
 export default config;
