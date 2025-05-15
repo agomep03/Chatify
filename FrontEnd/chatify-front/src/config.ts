8b36e2f5d51c3dd0625efd5f33ae11d5b5f89508
alicia <agomep03@estudiantes.unileon.es>
2025-05-15 09:38:24 +0200

    new experiment with env variables

--- FrontEnd/chatify-front/src/config.ts
+++ FrontEnd/chatify-front/src/config.ts
@@ -1,19 +1,6 @@
-// src/config.ts
-
-const isDevelopment = import.meta.env.MODE === "development";
-
-// Obtenemos y procesamos las URLs de producción
-const prodUrls: string[] = import.meta.env.VITE_API_PROD_URLS
-  ? import.meta.env.VITE_API_PROD_URLS.split(",").map((url: string) => url.trim())
-  : [];
 
 const config = {
-  apiBaseUrl: isDevelopment
-    ? import.meta.env.VITE_API_BASE_URL
-    : prodUrls.length > 0
-    ? prodUrls[0] // Selecciona la primera por defecto, o podrías implementar lógica para elegir otra
-    : "", // Fallback si no hay URL definida
+  apiBaseUrl: import.meta.env.VITE_API_BASE_URL || "https://chatifyback.onrender.com",
 };
-console.log(`API base URL configurada: ${config.apiBaseUrl || "No definida"}`);
-
+console.log("API URL:", config.apiBaseUrl);
 export default config;
