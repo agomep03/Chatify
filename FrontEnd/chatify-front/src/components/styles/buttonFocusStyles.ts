c2716054aa5d1697f5f3547da2ba4d4bc9a17f4a
alicia <agomep03@estudiantes.unileon.es>
2025-06-01 21:57:39 +0200

    boton no aparece circulo nblanco

---
+++ FrontEnd/chatify-front/src/components/styles/buttonFocusStyles.ts
@@ -0,0 +1,5 @@
+export const noFocusButtonSx = {
+  "&:focus": { outline: "none", border: "none", boxShadow: "none" },
+  "&:focus-visible": { outline: "none", border: "none", boxShadow: "none" },
+  "&:active": { outline: "none", border: "none", boxShadow: "none" },
+};
